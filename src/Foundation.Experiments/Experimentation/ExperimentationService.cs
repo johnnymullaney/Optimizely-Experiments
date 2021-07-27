39de71f0135984e9ce1b79100c04cf0cc40a4ddf
Jonathan Mullaney <jonathan.mullaney@madetoengage.com>
2021-07-27 14:13:26 +0100

    Added experimentation service to return optimizely decision and variables

---
+++ src/Foundation.Experiments/Experimentation/ExperimentationService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Web;
+using EPiServer.Logging;
+using EPiServer.ServiceLocation;
+using Foundation.Experiments.Core.Interfaces;
+using OptimizelySDK;
+using OptimizelySDK.OptimizelyDecisions;
+
+namespace Foundation.Experiments.Experimentation
+{
+    public class ExperimentationService : IExperimentationService
+    {
+        private readonly IUserRetriever _userRetriever;
+        private readonly IExperimentationFactory _experimentationFactory;
+        private readonly ILogger _logger;
+
+        public ExperimentationService(IUserRetriever userRetriever, IExperimentationFactory experimentationFactory)
+        {
+            _userRetriever = userRetriever;
+            _experimentationFactory = experimentationFactory;
+
+            ServiceLocator.Current.TryGetExistingInstance(out ILogger epiErrorLogger);
+            _logger = epiErrorLogger;
+        }
+
+        public OptimizelyDecision Decide(HttpContextBase httpContext, string key)
+        {
+            try
+            {
+                var userContext = GetOptimizelyUserContext(httpContext);
+
+                return userContext.Decide(key);
+            }
+            catch (Exception e)
+            {
+                _logger?.Log(Level.Error, $"Error thrown on Decide of Optimzely Experiment key: {key}", e);
+                return null;
+            }
+        }
+
+        public string GetFeatureVariableString(HttpContextBase httpContext, string featureKey, string variableKey)
+        {
+            var user = _userRetriever.GetUser(httpContext);
+
+            return _experimentationFactory.Instance.GetFeatureVariableString(featureKey, variableKey, user.UserId, user.UserAttributes);
+        }
+
+        private OptimizelyUserContext GetOptimizelyUserContext(HttpContextBase httpContext)
+        {
+            var user = _userRetriever.GetUser(httpContext);
+
+            return _experimentationFactory.Instance.CreateUserContext(user.UserId, user.UserAttributes);
+        }
+    }
+}
---
+++ src/Foundation.Experiments/Experimentation/IExperimentationService.cs
@@ -0,0 +1,11 @@
+﻿using System.Web;
+using OptimizelySDK.OptimizelyDecisions;
+
+namespace Foundation.Experiments.Experimentation
+{
+    public interface IExperimentationService
+    {
+        OptimizelyDecision Decide(HttpContextBase httpContext, string key);
+        string GetFeatureVariableString(HttpContextBase httpContext, string featureKey, string variableKey);
+    }
+}
